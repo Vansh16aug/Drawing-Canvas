19c01f6170a49583083c979c01a924eaa56f3e43
Vansh16aug <kumarvansh16aug@gmail.com>
2024-07-08 12:45:48 +0530

    added the feature of cloudinary

--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
-
+const cloudinary = require("../services/cloudinary");
 const Blog = require("../models/blog");
 
 const router = Router();
@@ -26,14 +26,31 @@ router.get("/add-new", (req, res) => {
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
   const { title, body } = req.body;
-  const blog = await Blog.create({
-    body,
-    title,
-    createdBy: req.user.id,
-    coverImageURL: `/uploads/${req.file.filename}`,
-  });
-  // console.log("body "+body);
-  return res.redirect(`/Eachblog/${blog._id}`);
+  try {
+    const result = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload(req.file.path, (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      });
+    });
+
+    const blog = await Blog.create({
+      body,
+      title,
+      createdBy: req.user.id,
+      coverImageURL: result.secure_url,
+    });
+
+    res.redirect(`/Eachblog/${blog._id}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Error uploading image",
+    });
+  }
 });
 
 module.exports = router;
